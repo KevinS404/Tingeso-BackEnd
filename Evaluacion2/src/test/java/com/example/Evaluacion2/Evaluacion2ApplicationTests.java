b1b95979e080237f4ae4660e9ab4db0fb7b42e51
Nicolas <nicolas.henriquez.t@usach.cl>
2022-01-04 18:58:43 -0300

    Spring Boot

---
+++ Evaluacion2/src/test/java/com/example/Evaluacion2/Evaluacion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Evaluacion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
